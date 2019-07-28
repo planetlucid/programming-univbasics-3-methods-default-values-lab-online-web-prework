4fb5a8725f6f1f66b132eb143a8e46c59af783e4
restful-strategy-4685 <planetlucid@gmail.com>
2019-07-28 03:09:21 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 # Your code here
 
-def meal_choice(cabbage,lettuce, protein = "meat")
+def meal_choice("cabbage","lettuce", protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{cabbage} and #{lettuce}."
 end
